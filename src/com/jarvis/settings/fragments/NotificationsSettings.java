98831c4d83b50a88b7c77e5ff9608998a3d7f337
Ali Hasan <ahb7671@gmail.com>
2023-01-12 17:44:10 +0500

    JS: option to pulse on new tracks [2/2]
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- src/com/jarvis/settings/fragments/NotificationsSettings.java
+++ src/com/jarvis/settings/fragments/NotificationsSettings.java
@@ -55,6 +55,8 @@ public class NotificationsSettings extends SettingsPreferenceFragment {
                 Settings.Secure.PULSE_AMBIENT_LIGHT_DURATION, 2, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.PULSE_AMBIENT_LIGHT_LAYOUT, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.PULSE_ON_NEW_TRACKS, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
